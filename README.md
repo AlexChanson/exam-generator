4a35052a5c9f56ec753f0a1cdb04d78076357a6c
Alexandre Chanson <a.chanson.tours@gmail.com>
2021-12-08 18:32:42 +0100

    repo

---
+++ README.md
@@ -0,0 +1,8 @@
+# A lazy Randomized Multiple Choice Questions Generator
+
+Thrown around in 30 minutes from random bits of code found in various documentations feel free to reuse it. 
+
+Input: one page pdf in data directory
+
+Output: a pdf file with a page ofr each student in your class
+
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from random import randint, choice
 from copy import deepcopy
 
 
+# change this to draw a random list of questions
 def get_questions():
     s = "A"
     s += str(randint(1, 6))
@@ -19,36 +20,34 @@ def get_questions():
     return s
 
 
-def create_watermark(input_pdf, output):
+def make_for_class(input_pdf, output, n_students, ):
     pdf_reader = PdfFileReader(input_pdf)
     pdf_writer = PdfFileWriter()
 
-    for i in range(80):
+    for i in range(n_students):
         questions = get_questions()
         page = deepcopy(pdf_reader.getPage(0))
 
+        # Questions top of the page
         packet = io.BytesIO()
         can = canvas.Canvas(packet, pagesize=letter)
-        can.drawString(270, 575, questions)
+        can.drawString(270, 575, questions) # Adjust position here
         can.save()
-        # create a new PDF with Reportlab
         new_text = PdfFileReader(packet)
-        # move to the beginning of the StringIO buffer
         packet.seek(0)
         page.mergePage(new_text.getPage(0))
 
+        # Questions answer section
         packet = io.BytesIO()
         can = canvas.Canvas(packet, pagesize=letter)
-        textobject = can.beginText(140, 320)
+        textobject = can.beginText(140, 320) # Adjust position here
         textobject.setFont("Helvetica", 12)
         for line in questions.split(","):
             textobject.textLine(line.strip() + " -->")
             textobject.textLine()
         can.drawText(textobject)
         can.save()
-        # create a new PDF with Reportlab
         new_text = PdfFileReader(packet)
-        # move to the beginning of the StringIO buffer
         packet.seek(0)
         page.mergePage(new_text.getPage(0))
 
@@ -59,6 +58,4 @@ def create_watermark(input_pdf, output):
 
 
 if __name__ == '__main__':
-    create_watermark(
-        input_pdf='data/input.pdf',
-        output='data/output.pdf')
+    make_for_class(input_pdf='data/input.pdf', output='data/output.pdf', n_students=80)
